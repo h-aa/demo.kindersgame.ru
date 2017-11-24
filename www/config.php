6b12cb34781455a9ca8707285928e08d8f7b4258
Khusainov Artur <pinicilin2006@ya.ru>
2017-11-24 17:50:33 +0500

    Отключил отображение неважных ошибок

--- www/config.php
+++ www/config.php
@@ -6,4 +6,5 @@ define("DBPASS", "WrLHyR9DePHypNTj");
 define("SALT",  "KLJAHSKDH&*^kasnhl");
 define("COMMENTLIMIT", "5");
 date_default_timezone_set("Europe/Moscow");
+ini_set('error_reporting', 'E_ALL ^ E_NOTICE');
 ?>
